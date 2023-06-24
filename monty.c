ab2e5d9d7dec49680574f432edf14111ced163b9
idrissjuanito <idrissjuanito@gmail.com>
2023-06-24 22:04:43 +0000

    trying to fix comment line error

--- monty.c
+++ monty.c
@@ -21,7 +21,7 @@ void run_instructions(FILE *fl, stack_t **stack)
 		{
 			line_num++;
 			opcode = parse_ln(line);
-			if (!opcode || strcmp(opcode, "nop") == 0 || line[0] == '#')
+			if (!opcode || strcmp(opcode, "nop") == 0 || opcode[0] == '#')
 				continue;
 			f = op_func(opcode);
 			if (!f)
