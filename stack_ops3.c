fc899635a8ea340983625b04a8154877898d0159
idrissjuanito <idrissjuanito@gmail.com>
2023-06-24 21:57:17 +0000

    fixed unused attribute

--- stack_ops3.c
+++ stack_ops3.c
@@ -32,7 +32,7 @@ void pchar_stack(stack_t **stack, unsigned int line_num)
  *
  * Return: nothing
  */
-void pstr_stack(stack_t **stack, unsigned int line_num)
+void pstr_stack(stack_t **stack, __attribute__((unused))unsigned int line_num)
 {
 	stack_t *tmp = *stack;
 
